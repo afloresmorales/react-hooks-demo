4908e83127aa6f101216efb2a4540c8d8ab49530
afloresmorales <afloresmorales96@gmail.com>
2020-07-02 13:44:52 -0600

    adding the react hooks and using function component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//"https://reactnative.dev/movies.json"
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [favoriteMovie, setFavoriteMovie] = useState('');
+  const [releaseYear, setReleaseYear] =  useState('');
+
+  useEffect(()=>{
+    fetch("https://reactnative.dev/movies.json")
+    .then(res => res.json())
+    .then(result => setMovies(result.movies))
+  }, [])
+
+  const addFavoriteMovieToList = () => {
+    const movieObject = {title: favoriteMovie, releaseYear};
+    const newMoviesList = [...movies, movieObject];
+    console.log(newMoviesList)
+    setMovies(newMoviesList);
+    setFavoriteMovie('');
+    setReleaseYear('');
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {
+            movies.map((movie, index)=> (
+            <li key={index}>{movie.title}, {movie.releaseYear}</li>
+            ))
+          }
+        </ul>
+        <div>
+          <input value={favoriteMovie} onChange={event => setFavoriteMovie(event.target.value)} placeholder='Favorite movie' />
+          <input value={releaseYear} onChange={event => setReleaseYear(event.target.value)} placeholder='Release Year' />
+          <button onClick={addFavoriteMovieToList}>Add</button>
+        </div>
       </header>
     </div>
   );
